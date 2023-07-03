36c4a91cbba18c093869d7e41a793c67f6038870
Daniel Baykov <danibaikov@gmail.com>
2023-07-03 16:13:33 +0300

    Initial commit

---
+++ src/main/java/baykov/daniel/springbootlibraryapp/SpringbootLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package baykov.daniel.springbootlibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLibraryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/baykov/daniel/springbootlibraryapp/SpringbootLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package baykov.daniel.springbootlibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
