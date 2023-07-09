b55eb565b9562e365637e94b036d55146fb57295
Daniel Baykov <danibaikov@gmail.com>
2023-07-09 14:49:41 +0300

    adding AuthorController and some dependencies

---
+++ src/main/java/baykov/daniel/springbootlibraryapp/controllers/AuthorController.java
@@ -0,0 +1,52 @@
+package baykov.daniel.springbootlibraryapp.controllers;
+
+import baykov.daniel.springbootlibraryapp.payload.dto.AuthorDTO;
+import baykov.daniel.springbootlibraryapp.payload.response.AuthorResponse;
+import baykov.daniel.springbootlibraryapp.services.AuthorService;
+import baykov.daniel.springbootlibraryapp.utils.AppConstants;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/authors")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AuthorDTO> createAuthor(@Valid @RequestBody AuthorDTO authorDTO) {
+        return new ResponseEntity<>(authorService.createAuthor(authorDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AuthorDTO> getAuthor(@PathVariable(name = "id") Long authorId) {
+        return ResponseEntity.ok(authorService.getAuthorById(authorId));
+    }
+
+    @GetMapping
+    public ResponseEntity<AuthorResponse> getAllAuthors(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIR, required = false) String sortDir) {
+        return ResponseEntity.ok(authorService.getAllAuthors(pageNo, pageSize, sortBy, sortDir));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<AuthorDTO> updateAuthorById(@PathVariable(name = "id") Long authorId,
+                                                      @Valid @RequestBody AuthorDTO authorDTO) {
+        return ResponseEntity.ok(authorService.updateAuthorById(authorDTO, authorId));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteAuthorById(@PathVariable(name = "id") Long authorId) {
+        authorService.deleteAuthorById(authorId);
+        return ResponseEntity.ok(AppConstants.AUTHOR_DELETE_MESSAGE);
+    }
+}
--- src/main/java/baykov/daniel/springbootlibraryapp/utils/AppConstants.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/utils/AppConstants.java
@@ -4,4 +4,9 @@ public class AppConstants {
 
     public static final long ISBN_LOWER_BOUND = 1000_0000_0000_0000L;
     public static final long ISBN_UPPER_BOUND = 9999_9999_9999_9999L;
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIR = "asc";
+    public static final String AUTHOR_DELETE_MESSAGE = "Author was deleted successfully!";
 }
