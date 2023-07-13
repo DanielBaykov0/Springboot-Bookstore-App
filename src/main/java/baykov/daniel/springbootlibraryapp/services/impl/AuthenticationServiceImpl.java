ab3b24274506d9d2305d07ff92b335af209425ac
Daniel Baykov <danibaikov@gmail.com>
2023-07-13 23:37:35 +0300

    implementing JWT bearer token authentication

--- src/main/java/baykov/daniel/springbootlibraryapp/services/impl/AuthenticationServiceImpl.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/services/impl/AuthenticationServiceImpl.java
@@ -1,16 +1,44 @@
 package baykov.daniel.springbootlibraryapp.services.impl;
 
+import baykov.daniel.springbootlibraryapp.payload.dto.LoginDTO;
+import baykov.daniel.springbootlibraryapp.payload.dto.RegisterDTO;
 import baykov.daniel.springbootlibraryapp.repositories.RoleRepository;
 import baykov.daniel.springbootlibraryapp.repositories.UserRepository;
+import baykov.daniel.springbootlibraryapp.security.JWTTokenProvider;
+import baykov.daniel.springbootlibraryapp.services.AuthenticationService;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthenticationServiceImpl {
+public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JWTTokenProvider jwtTokenProvider;
+
+    public AuthenticationServiceImpl(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JWTTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+
+    @Override
+    public String login(LoginDTO loginDTO) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken())
+        return null;
+    }
+
+    @Override
+    public String register(RegisterDTO registerDTO) {
+        return null;
+    }
 }
