ded2fd298333a3a04ce4831f203c1dd2213cc338
Daniel Baykov <danibaikov@gmail.com>
2023-07-16 23:37:29 +0300

    implementing Spring Doc for Controller classes

--- src/main/java/baykov/daniel/springbootlibraryapp/controller/AuthorController.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/controller/AuthorController.java
@@ -5,6 +5,10 @@ import baykov.daniel.springbootlibraryapp.payload.response.AuthorResponse;
 import baykov.daniel.springbootlibraryapp.service.AuthorService;
 import baykov.daniel.springbootlibraryapp.utils.AppConstants;
 import baykov.daniel.springbootlibraryapp.utils.Messages;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/authors")
+@Tag(name = "CRUD REST APIs for Author Resource")
 public class AuthorController {
 
     private final AuthorService authorService;
@@ -20,16 +25,49 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
+    @Operation(
+            summary = "Create Author REST API",
+            description = "Create Author REST API is used to save author into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @PostMapping
     public ResponseEntity<AuthorDTO> createAuthor(@Valid @RequestBody AuthorDTO authorDTO) {
         return new ResponseEntity<>(authorService.createAuthor(authorDTO), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Author By Id REST API",
+            description = "Get Author By Id REST API is used to get a single author from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @GetMapping("{id}")
     public ResponseEntity<AuthorDTO> getAuthor(@PathVariable(name = "id") Long authorId) {
         return ResponseEntity.ok(authorService.getAuthorById(authorId));
     }
 
+    @Operation(
+            summary = "Get All Authors REST API",
+            description = "Get All Authors REST API is used to fetch all the authors from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @GetMapping
     public ResponseEntity<AuthorResponse> getAllAuthors(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
@@ -39,18 +77,52 @@ public class AuthorController {
         return ResponseEntity.ok(authorService.getAllAuthors(pageNo, pageSize, sortBy, sortDir));
     }
 
+    @Operation(
+            summary = "Update Author REST API",
+            description = "Update Author REST API is used to update a particular author in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @PutMapping("{id}")
     public ResponseEntity<AuthorDTO> updateAuthorById(@PathVariable(name = "id") Long authorId,
                                                       @Valid @RequestBody AuthorDTO authorDTO) {
         return ResponseEntity.ok(authorService.updateAuthorById(authorDTO, authorId));
     }
 
+    @Operation(
+            summary = "Delete Author REST API",
+            description = "Delete Author REST API is used to delete author from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteAuthorById(@PathVariable(name = "id") Long authorId) {
         authorService.deleteAuthorById(authorId);
         return ResponseEntity.ok(Messages.AUTHOR_DELETE_MESSAGE);
     }
 
+    @Operation(
+            summary = "Get Author By First Name, Last Name Or Country REST API",
+            description = "Search Author By First Name, Last Name Or Country REST API " +
+                    "is used to search authors by first name, last name or country from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @GetMapping("search")
     public ResponseEntity<AuthorResponse> getAllAuthorsByAuthorFirstNameOrAuthorLastNameOrAuthorCountry(
             @RequestParam(value = "firstName", required = false) String firstName,
--- src/main/java/baykov/daniel/springbootlibraryapp/controller/BookController.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/controller/BookController.java
@@ -5,6 +5,10 @@ import baykov.daniel.springbootlibraryapp.payload.response.BookResponse;
 import baykov.daniel.springbootlibraryapp.service.BookService;
 import baykov.daniel.springbootlibraryapp.utils.AppConstants;
 import baykov.daniel.springbootlibraryapp.utils.Messages;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/books")
+@Tag(name = "CRUD REST APIs for Book Resource")
 public class BookController {
 
     private final BookService bookService;
@@ -20,16 +25,49 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @Operation(
+            summary = "Create Book REST API",
+            description = "Create Book REST API is used to save book into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @PostMapping
     public ResponseEntity<BookDTO> createBook(@Valid @RequestBody BookDTO bookDTO) {
         return new ResponseEntity<>(bookService.createBook(bookDTO), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Book REST API",
+            description = "Get Book REST API is used to get a particular book from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @GetMapping("{id}")
     public ResponseEntity<BookDTO> getBookById(@PathVariable(name = "id") Long paperBookId) {
         return ResponseEntity.ok(bookService.getBookById(paperBookId));
     }
 
+    @Operation(
+            summary = "Get All Books REST API",
+            description = "Get All Books REST API is used to get all books from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @GetMapping
     public ResponseEntity<BookResponse> getAllBooks(
             @RequestParam(name = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
@@ -39,18 +77,52 @@ public class BookController {
         return ResponseEntity.ok(bookService.getAllBooks(pageNo, pageSize, sortBy, sortDir));
     }
 
+    @Operation(
+            summary = "Update Book REST API",
+            description = "Update Book REST API is used to update a particular book in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @PutMapping("{id}")
     public ResponseEntity<BookDTO> updateBookById(@PathVariable(name = "id") Long paperBookId,
                                                   @Valid @RequestBody BookDTO bookDTO) {
         return ResponseEntity.ok(bookService.updateBookByBookId(bookDTO, paperBookId));
     }
 
+    @Operation(
+            summary = "Delete Book REST API",
+            description = "Delete Book REST API is used to delete a particular book from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteBookById(@PathVariable(name = "id") Long paperBookId) {
         bookService.deleteBookByBookId(paperBookId);
         return ResponseEntity.ok(Messages.BOOK_DELETE_MESSAGE);
     }
 
+    @Operation(
+            summary = "Get Books By Title, Genre, Description, Publication year or Author name REST API",
+            description = "Search Books By Title, Genre, Description, Publication year or Author name REST API " +
+                    "is used to search books by title, genre, description, publication year or author name in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @GetMapping("search")
     public ResponseEntity<BookResponse> getBooksByTitleOrByGenreOrByDescriptionOrByPublicationYearOrByAuthorName(
             @RequestParam(value = "title", required = false) String title,
