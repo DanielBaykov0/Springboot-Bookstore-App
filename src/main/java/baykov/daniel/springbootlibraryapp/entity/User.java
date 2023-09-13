7e26f1d3bb2b556f6d82ef1fd5e1c253b5afdada
Daniel Baykov <danibaikov@gmail.com>
2023-09-13 16:15:29 +0300

    modifying user entity

--- src/main/java/baykov/daniel/springbootlibraryapp/entity/User.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/entity/User.java
@@ -5,46 +5,94 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "user")
 public class User extends BaseEntity {
 
-    @Column(nullable = false, unique = true)
-    private String username;
+    @Column(nullable = false)
+    private String firstName;
 
     @Column(nullable = false)
-    private String password;
-    private String name;
-
-    @Transient
-    private long age;
-
-    @Column(nullable = false)
-    private LocalDate dateOfBirth;
-    private String gender;
-
-    @Column(nullable = false)
-    private String address;
-    private String city;
-    private String county;
+    private String lastName;
 
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(columnDefinition = "boolean default false")
-    private boolean confirmed;
+    @Column(nullable = false)
+    private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String city;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "library_card_id", referencedColumnName = "id")
+    private LibraryCard libraryCard;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private Boolean isActive;
+
+    private Boolean isEmailVerified;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private Set<Role> roles;
+
+    public User(User user, String password, Set<Role> roles) {
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.password = password;
+        this.birthDate = user.getBirthDate();
+        this.gender = user.getGender();
+        this.phoneNumber = user.getPhoneNumber();
+        this.address = user.getAddress();
+        this.country = user.getCountry();
+        this.city = user.getCity();
+        this.isEmailVerified = false;
+        this.isActive = true;
+        this.roles = roles;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum GenderEnum {
+        MALE("Male"),
+        FEMALE("Female"),
+        NON_BINARY("Non-Binary"),
+        PREFER_NOT_TO_SAY("Prefer Not to Say");
+
+        private final String displayName;
+    }
 }
