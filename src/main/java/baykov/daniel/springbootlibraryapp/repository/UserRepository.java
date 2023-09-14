7f02922fbc7d5c3e7de7e9fa1c2ae0978ae525cc
Daniel Baykov <danibaikov@gmail.com>
2023-09-14 13:34:26 +0300

    modifying user repository

--- src/main/java/baykov/daniel/springbootlibraryapp/repository/UserRepository.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/repository/UserRepository.java
@@ -1,43 +1,13 @@
 package baykov.daniel.springbootlibraryapp.repository;
 
 import baykov.daniel.springbootlibraryapp.entity.User;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Tag(name = "User Repository")
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Operation(
-            summary = "Get User By Username Or Email",
-            description = "Search User By Username Or Email is used to get a user from the database"
-    )
-    Optional<User> getByUsernameOrEmail(String username, String email);
+    Optional<User> findByEmailIgnoreCase(String email);
 
-    @Operation(
-            summary = "Check User By Username",
-            description = "Check User By Username is used to see if a user exists in the database"
-    )
-    Boolean existsByUsername(String username);
-
-    @Operation(
-            summary = "Check User By Email",
-            description = "Check User By Email is used to see if a user exists in the database"
-    )
-    Boolean existsByEmail(String email);
-
-    @Operation(
-            summary = "Update User's Confirm Status by Email",
-            description = "Update User's Confirm Status by Email is used to update user's confirm status by email"
-    )
-    @Transactional
-    @Modifying
-    @Query("UPDATE User u " +
-            "SET u.confirmed = TRUE WHERE u.email = ?1")
-    void confirmEmail(String email);
+    Boolean existsByEmailIgnoreCase(String email);
 }
