9b8bbb006f7f0d87799d41d97a46bb1b42fadaad
Daniel Baykov <danibaikov@gmail.com>
2023-09-14 14:06:17 +0300

    adding util class for property variables

---
+++ src/main/java/baykov/daniel/springbootlibraryapp/utils/PropertyVariables.java
@@ -0,0 +1,31 @@
+package baykov.daniel.springbootlibraryapp.utils;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class PropertyVariables {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration-milliseconds}")
+    private long jwtExpirationDate;
+
+    @Value("${app.jwtRefreshExpiration}")
+    private Long refreshTokenDurationMs;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Value("${redirect.uri}")
+    private String redirectUri;
+
+    @Value("${confirm.email.uri}")
+    private String confirmEmailUri;
+
+    @Value("${reset.password.uri}")
+    private String resetPasswordUri;
+}
