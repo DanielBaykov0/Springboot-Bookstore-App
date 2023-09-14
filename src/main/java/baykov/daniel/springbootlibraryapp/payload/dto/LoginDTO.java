520e626330c8d2e85bfbc0a544ca08e5ba1c9928
Daniel Baykov <danibaikov@gmail.com>
2023-09-14 13:32:00 +0300

    modifying login and register dto

--- src/main/java/baykov/daniel/springbootlibraryapp/payload/dto/LoginDTO.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/payload/dto/LoginDTO.java
@@ -1,19 +1,15 @@
 package baykov.daniel.springbootlibraryapp.payload.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class LoginDTO {
 
-    @NotEmpty(message = "Username or email should not be null or empty")
-    private String usernameOrEmail;
+    @NotEmpty(message = "Email should not be null or empty")
+    private String email;
 
     @NotEmpty(message = "Password should not be null or empty")
     private String password;
