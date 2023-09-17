5b0b8b5205659fa698b8cfe35fb1735b0072156f
Daniel Baykov <danibaikov@gmail.com>
2023-09-17 11:36:10 +0300

    modifying author entity

--- src/main/java/baykov/daniel/springbootlibraryapp/entity/Author.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/entity/Author.java
@@ -1,8 +1,6 @@
 package baykov.daniel.springbootlibraryapp.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,16 +18,38 @@ public class Author extends BaseEntity {
 
     @Column(nullable = false)
     private String firstName;
+
     private String lastName;
-    private String countryBorn;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "city_id")
+    private City city;
+
     private LocalDate birthDate;
+
     private Boolean isAlive;
+
     private LocalDate deathDate;
+
+    public Author(Author author, Country country, City city) {
+        this.firstName = author.getFirstName();
+        this.lastName = author.getLastName();
+        this.country = country;
+        this.city = city;
+        this.birthDate = author.getBirthDate();
+        this.isAlive = author.getIsAlive();
+        this.deathDate = author.getDeathDate();
+    }
 
     public void update(Author author) {
         this.firstName = author.getFirstName();
         this.lastName = author.getLastName();
-        this.countryBorn = author.getCountryBorn();
+        this.country = author.getCountry();
+        this.city = author.getCity();
         this.birthDate = author.getBirthDate();
         this.isAlive = author.getIsAlive();
         this.deathDate = author.getDeathDate();
