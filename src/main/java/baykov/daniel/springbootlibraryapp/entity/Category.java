85d6d07b4f2277a78de8ba9204e5c253037f70e7
Daniel Baykov <danibaikov@gmail.com>
2023-09-18 21:17:00 +0300

    adding category entity

---
+++ src/main/java/baykov/daniel/springbootlibraryapp/entity/Category.java
@@ -0,0 +1,21 @@
+package baykov.daniel.springbootlibraryapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
