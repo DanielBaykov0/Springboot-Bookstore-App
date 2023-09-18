0ed654d1d01c7e4b1169bdecac213cc709fd1cbc
Daniel Baykov <danibaikov@gmail.com>
2023-09-18 21:17:57 +0300

    modifying book dto

--- src/main/java/baykov/daniel/springbootlibraryapp/payload/dto/BookDTO.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/payload/dto/BookDTO.java
@@ -3,7 +3,8 @@ package baykov.daniel.springbootlibraryapp.payload.dto;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.URL;
+
+import java.math.BigDecimal;
 
 import static baykov.daniel.springbootlibraryapp.constant.AppConstants.CURRENT_YEAR;
 
@@ -15,12 +16,15 @@ public class BookDTO {
     @Size(min = 2, message = "Book title should have at least 2 characters")
     private String title;
 
-    @NotNull(message = "Author id cannot be null")
+    @NotNull(message = "Author should not be null")
     private Long authorId;
 
-    @NotEmpty(message = "Book genre should not be null or empty")
-    @Size(min = 4, message = "Book genre should have at least 4 characters")
-    private String genre;
+    @NotNull(message = "Book category should not be null")
+    private Long categoryId;
+
+    @NotEmpty(message = "Book language should not be null or empty")
+    @Size(min = 2, message = "Book language should have at least 2 characters")
+    private String language;
 
     @NotNull(message = "Book publication year cannot be null")
     @Min(value = 1400, message = "Book publication year should be between 1400 and 2023")
@@ -30,6 +34,10 @@ public class BookDTO {
     @NotEmpty(message = "Book description should not be null or empty")
     @Size(min = 10, message = "Book description should have at least 10 characters")
     private String description;
+
+    @NotNull(message = "Book number of pages cannot be null")
+    @Min(value = 1)
+    private Integer numberOfPages;
 
     @NotEmpty(message = "Book ISBN should not be null or empty")
     @Size(min = 13, max = 13, message = "Book ISBN should be 13 characters long")
@@ -43,9 +51,7 @@ public class BookDTO {
     @PositiveOrZero
     private Integer numberOfTotalCopies;
 
-    @URL
-    private String readingLink;
-
-    @URL
-    private String downloadLink;
+    @NotNull(message = "Book price cannot be null")
+    @Min(value = 1)
+    private BigDecimal price;
 }
