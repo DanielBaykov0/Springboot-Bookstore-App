ded12077a062518edbfd5204a8603c8471564491
Daniel Baykov <danibaikov@gmail.com>
2023-09-29 18:01:50 +0300

    refactoring audiobook entity

--- src/main/java/baykov/daniel/springbootlibraryapp/entity/Audiobook.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/entity/Audiobook.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -17,6 +18,10 @@ import java.util.List;
 @Table(name = "audiobook")
 @PrimaryKeyJoinColumn(name = "product_id")
 public class Audiobook extends Product {
+
+    @Column(nullable = false)
+    @Value(value = "1")
+    private Integer numberOfAvailableCopies;
 
     @Column(nullable = false)
     private BigDecimal duration;
@@ -49,11 +54,16 @@ public class Audiobook extends Product {
     @JoinColumn(nullable = false, name = "narrator_id")
     private Narrator narrator;
 
-    public void update(Audiobook audiobook, List<Author> authors, List<Category> categories, Narrator narrator) {
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<CommentReview> commentsReviews;
+
+    public Audiobook(Audiobook audiobook, List<Author> authors, List<Category> categories, Narrator narrator) {
         this.setTitle(audiobook.getTitle());
         this.setLanguage(audiobook.getLanguage());
+        this.setProductType(ProductTypeEnum.AUDIOBOOK);
         this.setPublicationYear(audiobook.getPublicationYear());
         this.setDescription(audiobook.getDescription());
+        this.setNumberOfAvailableCopies(1);
         this.setDuration(audiobook.getDuration());
         this.setISBN(audiobook.getISBN());
         this.setFileFormat(audiobook.getFileFormat());
@@ -62,5 +72,22 @@ public class Audiobook extends Product {
         this.setAuthors(authors);
         this.setCategories(categories);
         this.setNarrator(narrator);
+    }
+
+    public void update(Audiobook audiobook, List<Author> authors, List<Category> categories, Narrator narrator) {
+        if (audiobook.getTitle() != null) this.setTitle(audiobook.getTitle());
+        if (audiobook.getLanguage() != null) this.setLanguage(audiobook.getLanguage());
+        this.setProductType(ProductTypeEnum.AUDIOBOOK);
+        if (audiobook.getPublicationYear() != null) this.setPublicationYear(audiobook.getPublicationYear());
+        if (audiobook.getDescription() != null) this.setDescription(audiobook.getDescription());
+        this.setNumberOfAvailableCopies(1);
+        if (audiobook.getDuration() != null) this.setDuration(audiobook.getDuration());
+        if (audiobook.getISBN() != null) this.setISBN(audiobook.getISBN());
+        if (audiobook.getFileFormat() != null) this.setFileFormat(audiobook.getFileFormat());
+        if (audiobook.getFileSize() != null) this.setFileSize(audiobook.getFileSize());
+        if (audiobook.getPrice() != null) this.setPrice(audiobook.getPrice());
+        if (authors != null) this.setAuthors(authors);
+        if (categories != null) this.setCategories(categories);
+        if (narrator != null) this.setNarrator(narrator);
     }
 }
