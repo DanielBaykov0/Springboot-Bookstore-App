110219a79662f658dd530c5562946ee5f686fce7
Daniel Baykov <danibaikov@gmail.com>
2023-09-29 21:29:45 +0300

    adding toString and equals and hash code annotations to entities

--- src/main/java/baykov/daniel/springbootlibraryapp/entity/Category.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/entity/Category.java
@@ -3,15 +3,14 @@ package baykov.daniel.springbootlibraryapp.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "category")
 public class Category extends BaseEntity {
