7c3a7344e8c9098c0a99706d0cf46ab80b04199d
Daniel Baykov <danibaikov@gmail.com>
2023-10-01 20:59:38 +0300

    modifying role repository

--- src/main/java/baykov/daniel/springbootlibraryapp/repository/RoleRepository.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/repository/RoleRepository.java
@@ -1,18 +1,11 @@
 package baykov.daniel.springbootlibraryapp.repository;
 
 import baykov.daniel.springbootlibraryapp.entity.Role;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-@Tag(name = "Role Repository")
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    @Operation(
-            summary = "Get Role By Name",
-            description = "Search Role By Name is used to get a role from the database"
-    )
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(Role.RoleEnum name);
 }
