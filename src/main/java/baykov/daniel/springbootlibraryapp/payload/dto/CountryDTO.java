05211420ddd9c62549f7c3b055f3d3f153e02a91
Daniel Baykov <danibaikov@gmail.com>
2023-10-01 20:44:36 +0300

    modifying country dto and mapper

--- src/main/java/baykov/daniel/springbootlibraryapp/payload/dto/CountryDTO.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/payload/dto/CountryDTO.java
@@ -1,11 +1,17 @@
 package baykov.daniel.springbootlibraryapp.payload.dto;
 
+import baykov.daniel.springbootlibraryapp.validator.name.ValidName;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class CountryDTO {
 
+    @Schema(description = "Name of the country", example = "United States")
+    @ValidName(message = "Country should not be null or empty and should have at least 1 English character.")
     private String name;
 }
--- src/main/java/baykov/daniel/springbootlibraryapp/payload/mapper/CountryMapper.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/payload/mapper/CountryMapper.java
@@ -3,10 +3,13 @@ package baykov.daniel.springbootlibraryapp.payload.mapper;
 
 import baykov.daniel.springbootlibraryapp.entity.Country;
 import baykov.daniel.springbootlibraryapp.payload.dto.CountryDTO;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
+@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface CountryMapper {
 
     CountryMapper INSTANCE = Mappers.getMapper(CountryMapper.class);
