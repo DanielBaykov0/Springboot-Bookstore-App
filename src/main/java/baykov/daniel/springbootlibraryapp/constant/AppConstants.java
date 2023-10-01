8977e1fb17b3176bb9377062758bb95388a0bdcb
Daniel Baykov <danibaikov@gmail.com>
2023-10-01 20:57:15 +0300

    adding constant variables

---
+++ src/main/java/baykov/daniel/springbootlibraryapp/constant/AppConstants.java
@@ -0,0 +1,31 @@
+package baykov.daniel.springbootlibraryapp.constant;
+
+public class AppConstants {
+
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIR = "asc";
+    public static final String MESSAGE = "message";
+
+    public static final String USER = "User";
+    public static final String USER_PROFILE = "User Profile";
+    public static final String EMAIL = "email";
+    public static final String CATEGORY = "Category";
+    public static final String AUTHOR = "Author";
+    public static final String NARRATOR = "Narrator";
+    public static final String PRODUCT = "Product";
+    public static final String PRODUCT_TYPE = "Product type";
+    public static final String BOOK = "Book";
+    public static final String EBOOK = "Ebook";
+    public static final String AUDIOBOOK = "Audiobook";
+    public static final String COUNTRY = "Country";
+    public static final String CITY = "City";
+    public static final String TOKEN_TYPE = "TokenType";
+    public static final String CONFIRMATION_TOKEN = "Confirmation Token";
+
+    public static final String ID = "ID";
+    public static final String IDS = "IDs";
+    public static final String TYPE = "type";
+    public static final String TOKEN = "token";
+}
