18c85166fa723b48170e14e40dab8dafc285b399
Daniel Baykov <danibaikov@gmail.com>
2023-10-01 20:59:30 +0300

    modifying narrator repository

--- src/main/java/baykov/daniel/springbootlibraryapp/repository/NarratorRepository.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/repository/NarratorRepository.java
@@ -8,13 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface NarratorRepository extends JpaRepository<Narrator, Long> {
 
-    Page<Narrator> findAllByCountryId(Long countryId, Pageable pageable);
-
-    Page<Narrator> findAllByCityId(Long cityId, Pageable pageable);
-
-    @Query("SELECT n from Narrator n " +
-            "WHERE (:name IS NULL OR CONCAT(LOWER(n.firstName), ' ', LOWER(n.lastName)) LIKE %:name%) " +
+    @Query("SELECT n FROM Narrator n " +
+            "WHERE (:name IS NULL OR CONCAT(LOWER(n.firstName), ' ', LOWER(n.lastName)) LIKE CONCAT('%', :name, '%')) " +
             "AND (:countryId IS NULL OR n.country.id = :countryId) " +
-            "AND (:cityId IS NULL OR n.city.id = :cityId")
+            "AND (:cityId IS NULL OR n.city.id = :cityId)")
     Page<Narrator> findBySearchParams(String name, Long countryId, Long cityId, Pageable pageable);
 }
