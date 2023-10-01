fd0a9bdafe82cfe24178383acdece06154c449d9
Daniel Baykov <danibaikov@gmail.com>
2023-10-01 20:57:52 +0300

    adding author repository

--- src/main/java/baykov/daniel/springbootlibraryapp/repository/AuthorRepository.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/repository/AuthorRepository.java
@@ -8,13 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    Page<Author> findAllByCountryId(Long countryId, Pageable pageable);
-
-    Page<Author> findAllByCityId(Long cityId, Pageable pageable);
-
     @Query("SELECT a from Author a " +
-            "WHERE (:name IS NULL OR CONCAT(LOWER(a.firstName), ' ', LOWER(a.lastName)) LIKE %:name%) " +
-            "AND (:country IS NULL OR LOWER(a.country) LIKE %:country%) " +
-            "AND (:city IS NULL OR LOWER(a.city) LIKE %:city%)")
-    Page<Author> findBySearchParams(String name, String country, String city, Pageable pageable);
+            "WHERE (:name IS NULL OR CONCAT(LOWER(a.firstName), ' ', LOWER(a.lastName)) LIKE CONCAT('%', :name, '%')) " +
+            "AND (:countryId IS NULL OR a.country.id = :countryId) " +
+            "AND (:cityId IS NULL OR a.city.id = :cityId)")
+    Page<Author> findBySearchParams(String name, Long countryId, Long cityId, Pageable pageable);
 }
