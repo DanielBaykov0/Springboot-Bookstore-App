94fdc291a88d89f4976cad59198516e8aed2a7f9
Daniel Baykov <danibaikov@gmail.com>
2023-10-01 20:43:04 +0300

    modifying city dto modifying mapper

--- src/main/java/baykov/daniel/springbootlibraryapp/payload/mapper/CityMapper.java
+++ src/main/java/baykov/daniel/springbootlibraryapp/payload/mapper/CityMapper.java
@@ -2,11 +2,14 @@ package baykov.daniel.springbootlibraryapp.payload.mapper;
 
 import baykov.daniel.springbootlibraryapp.entity.City;
 import baykov.daniel.springbootlibraryapp.payload.dto.CityDTO;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
+@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface CityMapper {
 
     CityMapper INSTANCE = Mappers.getMapper(CityMapper.class);
